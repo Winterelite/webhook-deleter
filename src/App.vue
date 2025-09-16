8fa2a2de5369b93591f7907fe4deddc99e221985
Winterelite <jaunreleroux2012.2@gmail.com>
2025-09-16 12:04:19 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -42,10 +42,9 @@
 			</TransitionGroup>
 
 			<footer class="footer">
-				<p>Made with ❤️ by spreehertz
+				<p>Made with 🥀 by petabyte
 				</p>
-				<p class="contributor">Contributor:<a class="contributor_link" href="https://github.com/Fedox-die-Ente">Fedox-die-Ente</a></p>
-				<a href="https://github.com/spreehertz/webhook-deleter" target="_blank" rel="noopener noreferrer">
+				<a href="https://github.com/Winterelite/webhook-deleter" target="_blank" rel="noopener noreferrer">
 					<GithubIcon class="icon" />
 				</a>
 			</footer>
